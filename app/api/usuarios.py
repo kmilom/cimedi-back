8103052ebf71d1ae35715bbc32c007ec56acc666
kmilom <90485091+kmilom@users.noreply.github.com>
2024-05-08 20:12:17 -0500

    crud reorganized

---
+++ app/api/usuarios.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+from cruds.crudUsuario import *
+from cruds.crudEps import *
+
+router = APIRouter()
+
+@router.get("/usuarios/")
+def readAllUsuarios():
+    usuarios = getAllUsuarios()
+    return {"Usuarios": usuarios}
+
+@router.get("/usuarios/{idUsuario}")
+def readUsuarioById(idUsuario: int):
+    usuario = getUsuarioById(idUsuario)
+    return {"Usuario": usuario}
+
+@router.post("/crear-usuario")
+def createUsuario(usuario: Usuario):
+    if(crearUsuario(usuario)):
+        return {"Message": "Usuario creado exitosamente!"}
+    else:
+        return {"Message": "Error al crear el usuario!"}
+
+
+@router.put("/editar-usuario/{idUsuario}")
+def updateUsuario(idUsuario: int, usuario: Usuario):
+    usuario.idUsuario = idUsuario
+    if editarUsuario(usuario):
+        return {"Message": "Usuario editado correctamente!"}
+    else:
+        return {"Message": "No se pudo editar el usuario!"}
+    
+@router.delete("/eliminar-usuario/{idUsuario}")
+def deleteUsuario(idUsuario: int):
+    if(eliminarUsuario(idUsuario)):
+        return {"Message": "Usuario eliminado correctamente!"}
+    else:
+        return {"Message": "Error al eliminar el usuario"}
--- app/app.py
+++ app/app.py
@@ -1,43 +1,11 @@
 from fastapi import FastAPI
-from cruds.crudUsuario import *
-from cruds.crudEps import *
+from api.usuarios import router as usuariosRouter
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return RedirectResponse(url = "/docs")
 
-@app.get("/usuarios")
-def readAllUsuarios():
-    usuarios = getAllUsuarios()
-    return {"Usuarios": usuarios}
-
-@app.get("/usuarios/{idUsuario}")
-def readUsuarioById(idUsuario: int):
-    usuario = getUsuarioById(idUsuario)
-    return {"Usuario": usuario}
-
-@app.post("/crear-usuario")
-def createUsuario(usuario: Usuario):
-    if(crearUsuario(usuario)):
-        return {"Message": "Usuario creado exitosamente!"}
-    else:
-        return {"Message": "Error al crear el usuario!"}
-
-
-@app.put("/editar-usuario/{idUsuario}")
-def updateUsuario(idUsuario: int, usuario: Usuario):
-    usuario.idUsuario = idUsuario
-    print(usuario)
-    if editarUsuario(usuario):
-        return {"Message": "Usuario editado correctamente!"}
-    else:
-        return {"Message": "No se pudo editar el usuario!"}
-    
-@app.delete("/eliminar-usuario/{idUsuario}")
-def deleteUsuario(idUsuario: int):
-    if(eliminarUsuario(idUsuario)):
-        return {"Message": "Usuario eliminado correctamente!"}
-    else:
-        return {"Message": "Error al eliminar el usuario"}
+app.include_router(usuariosRouter, prefix = "/api")
